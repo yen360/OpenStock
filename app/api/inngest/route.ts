aa58d2dc17f01f70ef8079aad3846709c191d121
Steven Tan <18841105+yen360@users.noreply.github.com>
2026-09-24 16:11:25 +0800

    Register Telegram failure notifier

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,9 +1,9 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { sendWeeklyNewsSummary, sendSignUpEmail, checkStockAlerts, checkInactiveUsers } from "@/lib/inngest/functions";
-import { notifyFailuresToN8n } from "@/lib/inngest/notify";
+import { notifyFailuresToTelegram } from "@/lib/inngest/notify";
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    functions: [sendSignUpEmail, sendWeeklyNewsSummary, checkStockAlerts, checkInactiveUsers, notifyFailuresToN8n],
+    functions: [sendSignUpEmail, sendWeeklyNewsSummary, checkStockAlerts, checkInactiveUsers, notifyFailuresToTelegram],
 })
